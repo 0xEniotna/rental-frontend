b3da96c2c20aa535f7affc405aa01115d1b2bc07
0xKarasu <0xKarasu@protonmail.com>
2023-01-17 23:45:18 +0100

    small changes

--- store.ts
+++ store.ts
@@ -6,6 +6,8 @@ import {
     RematchRootState,
 } from "@rematch/core";
 import { useDispatch, useSelector } from "react-redux";
+import { useEffect, useState } from 'react'
+
 
 type SetAccountType = {
     account: string;
@@ -40,13 +42,14 @@ export const state = createModel<RootModel>()({
         },
         setNetwork(state, network: string) {
             const newState: any = { ...state, network };
-            const networkMessage = `please connect to the ${process.env.NEXT_PUBLIC_STARKNET_NETWORK} network`;
+            // const networkMessage = `please connect to the ${process.env.NEXT_PUBLIC_STARKNET_NETWORK} network`;
             if (network) {
-                if (network !== process.env.NEXT_PUBLIC_STARKNET_NETWORK) {
-                    newState.message = networkMessage;
-                } else if (state.message === networkMessage) {
-                    newState.message = "";
-                }
+                newState.network = network;
+                // if (network !== process.env.NEXT_PUBLIC_STARKNET_NETWORK) {
+                //     newState.message = networkMessage;
+                // } else if (state.message === networkMessage) {
+                //     newState.message = "";
+                // }
             }
             return newState;
         },
@@ -67,26 +70,24 @@ export const store = init({
     models,
 });
 
-setTimeout(() => {
-    if (typeof window !== "undefined") {
-        const previousAccount = localStorage.getItem("account");
-
-        if (previousAccount) {
-            store.dispatch({
-                type: "state/setAccount",
-                payload: {
-                    account: previousAccount,
-                    accountConnected: false,
-                },
-            });
-        }
-        store.dispatch({
-            type: "state/setRehydrated",
-            payload: true,
-        });
-    }
-}, 100);
-
+// setTimeout(() => {
+//     if (typeof window !== "undefined") {
+//         const previousAccount = localStorage.getItem("account");
+//         if (previousAccount) {
+//             store.dispatch({
+//                 type: "state/setAccount",
+//                 payload: {
+//                     account: previousAccount,
+//                     accountConnected: false,
+//                 },
+//             });
+//         }
+//         store.dispatch({
+//             type: "state/setRehydrated",
+//             payload: true,
+//         });
+//     }
+// }, 100);
 
 export type Store = typeof store;
 export type Dispatch = RematchDispatch<RootModel>;
--- utils.ts
+++ utils.ts
@@ -1,16 +1,16 @@
 import { useStarknet, useAccount } from "@starknet-react/core";
 import { useEffect, useRef } from "react";
-import { uint256 } from "starknet";
-import { toBN, toHex } from "starknet/dist/utils/number";
-import {
-  decodeShortString,
-  encodeShortString,
-} from "starknet/dist/utils/shortString";
+import { uint256, number, shortString } from "starknet";
 
 export const getAddressFromBN = (bn: any) => {
   const hexString = bn.toString(16);
   const prefix = "0".repeat(64 - hexString.length);
   return `0x${prefix}${hexString}`;
+};
+
+export const getAddressFromString = (str: String) => {
+  const prefix = "0x";
+  return `${prefix}${str}`;
 };
 
 export const getNumberFromUint = (uint: any) => {
@@ -32,11 +32,38 @@ export const useStarknetNetwork = () => {
     if (baseUrl.includes("alpha-mainnet.starknet.io")) {
       return "mainnet";
     } else if (baseUrl.includes("alpha4.starknet.io")) {
-      return "goerli";
+      return "SN_GOERLI";
     } else if (baseUrl.includes("alpha4-2.starknet.io")) {
       return "SN_GOERLI2";
     } else if (baseUrl.match(/^https?:\/\/localhost.*/)) {
       return "localhost";
+    }
+  } catch { }
+};
+
+export const starknetEndpoint = (network: string) => {
+  if (!network) {
+    return;
+  }
+  try {
+    switch (network) {
+      case "mainnet": {
+        return `https://starknet-mainnet.infura.io/v3/${process.env.NEXT_INFURA_GOERLI2}`
+      }
+      case "SN_GOERLI": {
+        return `https://starknet-goerli.infura.io/v3/${process.env.NEXT_INFURA_GOERLI2}`
+      }
+      case "SN_GOERLI2": {
+        return `https://starknet-goerli2.infura.io/v3/${process.env.NEXT_INFURA_GOERLI2}`
+      }
+      case "localhost": {
+        console.log("starknet endpoint: localhost");
+      }
+      default: {
+        //statements; 
+        console.log("starknet endpoint: none");
+        break;
+      }
     }
   } catch { }
 };
@@ -55,14 +82,14 @@ export const feltArrayToStr = (feltArray: number[]) => {
     if (n === 0) {
       return "";
     }
-    return decodeShortString(toHex(toBN(n)));
+    return shortString.decodeShortString(number.toHex(number.toBN(n)));
   });
 };
 
 export const strToFeltArray = (str: string): string[] => {
   const feltStrings = str.match(/.{1,31}/g);
   if (!feltStrings) return [];
-  const felts = feltStrings.map((s) => toBN(encodeShortString(s)).toString());
+  const felts = feltStrings.map((s) => number.toBN(shortString.encodeShortString(s)).toString());
   return felts;
 };
 
@@ -70,7 +97,7 @@ export const getExecuteParameterFromTheme = (theme: string): string[] => {
   const feltArray = [];
   try {
     for (const letter of theme) {
-      feltArray.push(toBN(encodeShortString(letter)).toString());
+      feltArray.push(number.toBN(shortString.encodeShortString(letter)).toString());
     }
   } catch (e) {
     // console.error(e);
