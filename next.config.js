bf5989bea999e2c1160c37d58ca035985e02fef3
0xKarasu <0xKarasu@protonmail.com>
2023-01-17 23:42:22 +0100

    :wrench:

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 /** @type {import('next').NextConfig} */
-
+const webpack = require('webpack');
 require("dotenv").config;
 
 const nextConfig = {
@@ -7,21 +7,28 @@ const nextConfig = {
   swcMinify: true,
   env: {
     NEXT_PUBLIC_ERC721_ADDRESS: process.env.NEXT_PUBLIC_ERC721_ADDRESS,
-    API_KEY: process.env.API_KEY,
-    API_KEY: process.env.API_KEY,
+    NEXT_INFURA_GOERLI2: process.env.NEXT_INFURA_GOERLI2,
+    NEXT_PUBLIC_ETH_ERC20_ADDRESS: process.env.NEXT_PUBLIC_ETH_ERC20_ADDRESS,
     API_KEY: process.env.API_KEY,
     API_KEY: process.env.API_KEY,
   },
+
   images: {
-    formats: ['image/avif', 'image/webp'],
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'cdn-testnet.aspect.co',
-        port: '',
-        pathname: '/contracts/custom/**/**',
-      },
-    ],
+    domains: ['cdn-testnet.aspect.co', 'ipfs.io', 'imagedelivery.net'],
+    // formats: ['image/avif', 'image/webp'],
+    // remotePatterns: [
+    //   {
+    //     protocol: 'https',
+    //     hostname: 'cdn-testnet.aspect.co',
+    //     port: '',
+    //     pathname: '/contracts/custom/**/**',
+    //   },
+    //   {
+    //     protocol: 'https',
+    //     hostname: 'cdn-testnet.aspect.co',
+    //     port: '',
+    //     pathname: '/images/**',
+    //   },
   },
 }
 
